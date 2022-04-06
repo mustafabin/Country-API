56c47a422d387214145f17cc15e116cea6e2432a
Mustafa <mbin828@gmail.com>
2022-04-06 14:20:25 -0400

    fixed doc link

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,8 @@ import countryRoutes from "./countryRoute.js";
 const router = Router();
 router.get("/", (req, res) => {
   res.json({
-    message: "incorrect end point look up the documentation here: 'deez' ",
+    message:
+      "incorrect end point look up the documentation here: 'https://github.com/mustafabin/Country-API' ",
   });
 });
 router.use("/countries", countryRoutes);
